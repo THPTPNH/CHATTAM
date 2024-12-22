da90bbdb77ea09aa3ac4225a7e56e34bda46138f
THPTPNH <phamtienquy032@gmail.com>
2024-12-22 22:17:55 +0700

    updaet

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let userMessage = null; // Variable to store user's message
 const inputInitHeight = chatInput.scrollHeight;
 
 // API configuration
-const API_KEY = "AIzaSyDaR0q0kKNOqfHOcJTeav0XpkvYcdPowBU"; // Your API key here
-const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=GEMINI_API_KEY";
+//const API_KEY = "AIzaSyDaR0q0kKNOqfHOcJTeav0XpkvYcdPowBU"; // Your API key here
+const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAWI4jAOb9f6zUULnyyV8_oIcb46vXA1f8";
 const createChatLi = (message, className) => {
   // Create a chat <li> element with passed message and className
   const chatLi = document.createElement("li");
